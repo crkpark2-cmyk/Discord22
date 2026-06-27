233aa3557fa2cb393c4365449bdf86fc96820174
crkpark2-cmyk <crkpark2@gmail.com>
2026-06-27 18:21:17 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@ intents = discord.Intents.default()
 intents.voice_states = True
 intents.guilds = True
 intents.members = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
